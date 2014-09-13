34104323098d0ac6c73def9a1c95ff01fccc04c0
Brittan McGinnis <konamax123@gmail.com>
2014-09-12 18:18:58 -0700

    changed routes and added a migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
-Rails.application.routes.draw do
+Shareup::Application.routes.draw do
+
+  namespace :api do 
+    resources :shares
+  end
+
+  get '/dashboard' => 'welcome#dashboard'
+  root to: 'welcome#index'
+
   devise_for :users,
-    :controllers => {
-      :omniauth_callbacks => "users/omniauth_callbacks"
-    }
+  :controllers => {
+    :omniauth_callbacks => "users/omniauth_callbacks"
+  }
 
 end
